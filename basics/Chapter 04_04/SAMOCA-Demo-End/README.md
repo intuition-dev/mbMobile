7c124981f1a5d3af92b2ae37efb14a9e7a71a8d0
unknown <uptimevic@gmail.com>
2018-05-31 18:37:57 -0400

    bas

---
+++ basics/Chapter 04_04/SAMOCA-Demo-End/README.md
@@ -0,0 +1,2 @@
+# SAMOCA-Demo
+The sample app for use in PhoneGap Build: First Look
---
+++ basics/Chapter 04_08/SAMOCA-Demo-End/js/map.js
@@ -0,0 +1,20 @@
+var map;
+		  
+/*$(document).on('pageinit', '#mapScreen', function(){  
+	map = L.map('map'); 				L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+			 
+	map.on('locationfound', onLocationFound);
+	map.on('locationerror', onLocationError);
+			 
+	map.locate({setView: true, maxZoom: 16});    
+});
+		  
+function onLocationFound(e) {
+	L.marker(e.latlng).addTo(map);
+}
+
+function onLocationError(e) {
+	alert(e.message);
+}*/
