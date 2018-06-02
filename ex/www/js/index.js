f39659916577936b36b2d8c5ef30f46271581f55
vic <administrator@2509.local>
2018-06-02 11:53:06 -0700

    ok

--- ex/www/js/index.js
+++ ex/www/js/index.js
@@ -1,49 +1,4 @@
 
-// http://github.com/logzio/logzio-js
-function initLog() {
-	const LogzioLogger = function(apiKey, sendConsoleJsErrors) {
-		this.key = apiKey
-		if (sendConsoleJsErrors) sendConsoleErrors()
-	}
-
-	let sendConsoleErrors = function() {
-	window.onerror = function (msg, url, line, col) {
-		LogzioLogger.log({
-			message: msg,
-			url: url,
-			line: line,
-			col: col
-		})
-	}
-	}
-
-	LogzioLogger.prototype.log = function(data) {
-		try {
-			let parsedMsg = typeof data == 'object' ? data : { message:data }
-			let logUrl = window.location.protocol + '//listener.logz.io:'
-			logUrl += (window.location.protocol === 'http:' ? '8090' : '8091') + '?token=' + this.key
-			Object.keys(parsedMsg).forEach(function(key) {
-				logUrl += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(parsedMsg[key])
-			})
-			let logImg = new Image()
-			logImg.src = logUrl
-		} catch (ex) {
-			if (window && window.console && typeof window.log.log == 'function')
-				log.log("Failed to send log because of exception:\n" + ex)
-				try {
-					console.log(ex)
-				} catch (err) {}
-		}
-	}
-	window.LogzioLogger = LogzioLogger
-
-	window.log = new LogzioLogger('JQMYDEDILZCNAlWPIbiSyyWaroBvfKSa')
-
-	log.log('Hello, this is just a test: 603')
-
-}
-/////////////////////////////////////////////////////////////////
-
 document.addEventListener('deviceready', onDeviceReady, false)
 
 function cssLoaded() {// called by the style sheet in layout
@@ -51,10 +6,9 @@ function cssLoaded() {// called by the style sheet in layout
 }
 
 function onDeviceReady() { // nothing will work before this
-	initLog() // do a test log
-
-	loadjs('//cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/components/sidebar.min.js', function() {
-		log.log('load js')
+	console.log('ready')
+	loadjs('https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/components/sidebar.min.js', function() {
+		console.log('loaded')
 	})
 }
 
