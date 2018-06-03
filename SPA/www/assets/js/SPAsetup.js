b372a924a40338153a148a6546df745c3e70121a
unknown <uptimevic@gmail.com>
2018-06-02 20:54:09 -0400

    adobe

--- SPA/www/assets/js/SPAsetup.js
+++ SPA/www/assets/js/SPAsetup.js
@@ -12,17 +12,39 @@
 }
 console.log('SPAsetup', "v2.06.1")
 ///////////////////////////////////////////////////////////////////////////////////
-// ie has 0 market share of mobile
+//11 ie has 0% market share of mobile
 
-loadjs([ 'https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/components/sidebar.min.js'
-	,'https://cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
-	//,'//cdn.jsdelivr.net/npm/intersection-observer@0.5.0/intersection-observer.js'
-	,'https://rawgit.com/metabake/SPA/master/SPA/www/router/spa-router.js'
-	,'https://unpkg.com/ionicons@4.1.2/dist/css/ionicons.min.css' // http://ionicons.com/usage
+document.addEventListener('deviceready', onDeviceReady, false)
 
-], 'cssJs', {
-	async: false //required due to loadjs bug with bundles
+$(document).ready(function() {
+	// are we running in native app or in a browser?
+	window.isphone = false
+	if(document.URL.indexOf("http://") === -1
+		 && document.URL.indexOf("https://") === -1) {
+		 window.isphone = true
+	}
+
+	window.isphone = false // REMOVE this line to support PhoneGap
+
+	console.log(window.isphone)
+	if( window.isphone ) { // //file is a browser
+		 document.addEventListener("deviceready", onDeviceReady, false)
+	} else {
+		 onDeviceReady()
+	}
 })
+
+function onDeviceReady() { // nothing will work before this
+	loadjs([ 'https://cdn.jsdelivr.net/npm/semantic-ui@2.3.1/dist/components/sidebar.min.js'
+		,'https://cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
+		//,'//cdn.jsdelivr.net/npm/intersection-observer@0.5.0/intersection-observer.js'
+		,'https://rawgit.com/metabake/SPA/master/SPA/www/router/spa-router.js'
+		,'https://unpkg.com/ionicons@4.1.2/dist/css/ionicons.min.css' // http://ionicons.com/usage
+
+	], 'cssJs', {
+		async: false //required due to loadjs bug with bundles
+	})
+}
 
 function cssLoaded() {// called by the style sheet in layout
 	console.log('css loaded', Date.now()-_start)
@@ -39,8 +61,9 @@ loadjs.ready(['css', 'cssJs'], function () {
 loadjs.ready(['style'], function () {// 'show' page, ex: unhide
 	console.log('style done', Date.now()-_start)
 
+
 	// SPA Lab:
-	loadjs('/assets/js/fx.js',
+	loadjs('https://rawgit.com/metabake/SPA/master/SPA/www/assets/js/fx.js',
 		'fxLoded', {
 		async: false //required due to loadjs bug with bundles
 	})
