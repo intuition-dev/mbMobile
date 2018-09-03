ea204c2b16bf23c5399f7720c7e583a264245a90
unknown <uptimevic@gmail.com>
2018-09-03 14:44:16 -0400

    typo

--- PGap/www/assets/router/spa-router.js
+++ PGap/www/assets/router/spa-router.js
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////
 // License LGPL v2.1, Copyright Metabake.org | Wolfgang | Cekvenich
 
-// requires script jquery, https://unpkg.com/axios@0.18.0/dist/axios.min.js
+// requires jquery, https://unpkg.com/axios@0.18.0/dist/axios.min.js
 // and https://cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
 
 $(window).on('popstate', function (e) {//back/forward button
