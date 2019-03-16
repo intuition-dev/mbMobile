fedec50ac74f14c626869f93be75a54930cb3b00
Vic <uptimevic@gmail.com>
2019-03-15 20:24:12 -0400

    is js

--- elect/app/www/assets/js/setup.js
+++ elect/app/www/assets/js/setup.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 loadjs([
 
     'https://cdn.jsdelivr.net/npm/blueimp-load-image@2.19.0/js/load-image.all.min.js'
-   , 'https://cdn.jsdelivr.net/npm/is_js@0.9.0/is.min.js'
+   
 
    , 'https://cdn.jsdelivr.net/npm/tabulator-tables@4.1.3/dist/js/tabulator.min.js'
    , 'https://cdn.jsdelivr.net/npm/tabulator-tables@4.1.3/dist/css/tabulator.min.css'
